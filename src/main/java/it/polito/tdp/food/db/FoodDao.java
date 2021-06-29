3edadb4d44eb2b9b3e2bfa8f012dbc09b4bb0bc3
utente <utente@DESKTOP-UJ3OII6.lan>
2021-06-29 19:14:01 +0200

    uanm

--- src/main/java/it/polito/tdp/food/db/FoodDao.java
+++ src/main/java/it/polito/tdp/food/db/FoodDao.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.food.model.Condiment;
 import it.polito.tdp.food.model.Food;
 import it.polito.tdp.food.model.Portion;
@@ -17,11 +20,11 @@ public class FoodDao {
 			Connection conn = DBConnect.getConnection() ;
 
 			PreparedStatement st = conn.prepareStatement(sql) ;
-			
+
 			List<Food> list = new ArrayList<>() ;
-			
+
 			ResultSet res = st.executeQuery() ;
-			
+
 			while(res.next()) {
 				try {
 					list.add(new Food(res.getInt("food_code"),
@@ -31,7 +34,7 @@ public class FoodDao {
 					t.printStackTrace();
 				}
 			}
-			
+
 			conn.close();
 			return list ;
 
@@ -41,18 +44,18 @@ public class FoodDao {
 		}
 
 	}
-	
+
 	public List<Condiment> listAllCondiments(){
 		String sql = "SELECT * FROM condiment" ;
 		try {
 			Connection conn = DBConnect.getConnection() ;
 
 			PreparedStatement st = conn.prepareStatement(sql) ;
-			
+
 			List<Condiment> list = new ArrayList<>() ;
-			
+
 			ResultSet res = st.executeQuery() ;
-			
+
 			while(res.next()) {
 				try {
 					list.add(new Condiment(res.getInt("condiment_code"),
@@ -64,7 +67,7 @@ public class FoodDao {
 					t.printStackTrace();
 				}
 			}
-			
+
 			conn.close();
 			return list ;
 
@@ -73,42 +76,67 @@ public class FoodDao {
 			return null ;
 		}
 	}
-	
-	public List<Portion> listAllPortions(){
+
+	public void listAllPortions(Map<Integer,Portion>idMap){
 		String sql = "SELECT * FROM portion" ;
 		try {
 			Connection conn = DBConnect.getConnection() ;
 
 			PreparedStatement st = conn.prepareStatement(sql) ;
-			
-			List<Portion> list = new ArrayList<>() ;
-			
+
+
 			ResultSet res = st.executeQuery() ;
-			
+
 			while(res.next()) {
 				try {
-					list.add(new Portion(res.getInt("portion_id"),
-							res.getDouble("portion_amount"),
-							res.getString("portion_display_name"), 
-							res.getDouble("calories"),
-							res.getDouble("saturated_fats"),
-							res.getInt("food_code")
-							));
-				} catch (Throwable t) {
+					if(!idMap.containsKey(res.getInt("portion_id"))) {
+						Portion p =	new Portion(res.getInt("portion_id"),
+								res.getDouble("portion_amount"),
+								res.getString("portion_display_name"), 
+								res.getDouble("calories"),
+								res.getDouble("saturated_fats"),
+								res.getInt("food_code")
+								);
+						idMap.put(res.getInt("portion_id"), p);
+					}
+				}catch (Throwable t) {
 					t.printStackTrace();
 				}
 			}
-			
+
+
 			conn.close();
-			return list ;
+
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null ;
+
 		}
 
 	}
-	
-	
+
+	public List<Portion> getVertici(Map<Integer,Portion>idMap,double soglia){
+
+		String sql="SELECT distinct portion_id " + 
+				"FROM `portion` " + 
+				"WHERE calories<?";
+		List<Portion> result = new LinkedList<>();
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setDouble(1, soglia);
+			ResultSet res = st.executeQuery();
+			while(res.next()) {
+				result.add(idMap.get(res.getInt("portion_id")));
+			}
+			conn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return result;
+	}
+
+
 
 }
--- src/main/java/it/polito/tdp/food/model/Model.java
+++ src/main/java/it/polito/tdp/food/model/Model.java
@@ -1,8 +1,23 @@
 package it.polito.tdp.food.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import it.polito.tdp.food.db.FoodDao;
 
 public class Model {
+	private Map<Integer,Portion>idMap;
+	private Graph <Portion,DefaultWeightedEdge> grafo;
+	private FoodDao dao;
 	
+	public Model() {
+		dao = new FoodDao();
+		idMap = new HashMap<Integer,Portion>();
+		dao.listAllPortions(idMap);
+	}
 	
 	
 }
